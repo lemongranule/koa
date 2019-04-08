cc5f13056a005b1f414a91db36cfe01847760d74
tttell <linyk@tttell.com>
2019-04-08 13:01:10 +0800

    添加 跨域配置

--- main.js
+++ main.js
@@ -2,17 +2,29 @@
 
 
 const cluster = require('cluster');
+
 const Koa = require('koa');
 
 const Router = require('koa-router')  //路由依赖的中间间
-const bodyParser = require('koa-bodyparser')
+
+const bodyParser = require('koa-bodyparser')//接受post请求的body 数据
+
+
+
+const cors = require('@koa/cors');//跨域
+
+
 // 配置
 const config = require('./config');
 const packageConfig = require('./package.json');
 config['version'] = packageConfig['version'];
 
 
+
 const app = new Koa();
+
+
+app.use(cors());//跨域配置
 
 
 
@@ -22,6 +34,7 @@ const app = new Koa();
 
 
 
+//路由
 app.use(require('./routes/root').routes());
 app.use(require('./routes/index').routes());
 
